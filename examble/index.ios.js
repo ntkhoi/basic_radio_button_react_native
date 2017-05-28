81581f6302e9138c03161830f9f27cea1a5690dd
JamesNguyen <ntkhoiptit@gmail.com>
2017-05-28 17:32:40 +0700

    very simple radio button component for react native

---
+++ examble/index.ios.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import RadioButton from './radiobutton.js'
+
+export default class dustin_radio_button extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <RadioButton> </RadioButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('dustin_radio_button', () => dustin_radio_button);
---
+++ radiobutton.js
@@ -0,0 +1,124 @@
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,TouchableOpacity,
+  Text,
+  View, Alert
+} from 'react-native';
+
+class RadioButton extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      isSelected: this.props.isSelected,
+      
+    };
+
+    console.log("call contructer");
+
+    
+  }
+  static defaultProps = {
+      isSelected: false,
+      
+  }
+ _changeRadioState(){           
+     console.log("this.props.onPress(this.props.index) ;" + this.props.index);
+    this.props.onPress(this.props.index) ;
+
+    this.setState({
+        isSelected: true
+    })
+ }
+ componentWillReceiveProps(){
+     console.log(this.props.isSelected + "componentWillReceiveProps");
+    this.setState({
+        isSelected: this.props.isSelected
+    })
+ }
+ 
+ render() {
+     console.log( "render radio nutton: " + this.state.isSelected);
+    return (
+        
+        <TouchableOpacity onPress={() => this._changeRadioState()}>
+            <View style={[{
+                height: 24,
+                width: 24,
+                borderRadius: 12,
+                borderWidth: 1,
+                borderColor: '#A9A9A9',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }, this.props.style]}>
+                {
+                this.state.isSelected ?
+                    <View style={{
+                    height: 12,
+                    width: 12,
+                    borderRadius: 6,
+                    backgroundColor: '#6495ED',
+                    }}/>
+                    : null
+                }
+            </View>
+        </TouchableOpacity>
+    );
+  }
+}
+
+
+class RadioControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        is_active_at_index : this.props.index_active,
+    };
+    this._renderRadioBtn = this._renderRadioBtn.bind(this)
+  }
+  static defaultProps = {
+      radio_props: [1,2,3,4],
+      index_active: 0,
+  }
+ 
+_radioButtonPress(index){
+   
+   this.setState({is_active_at_index: index}, function () {
+    this.setState({})
+    });
+}
+
+_renderRadioBtn(obj, i) {
+    
+   console.log( this.state.is_active_at_index + "")
+    return (
+      <RadioButton
+            key={i}
+            index={i}
+            isSelected={this.state.is_active_at_index === i}
+            onPress={(index) =>{ this._radioButtonPress(index) ;                        
+            }}
+      />
+    )
+  }
+
+  render() {
+    console.log("rerender");
+    var render_content = false;
+    if (this.props.radio_props.length) {
+      render_content = this.props.radio_props.map(this._renderRadioBtn)
+      
+    }
+
+    return (
+        <View>
+          {render_content}
+        </View>
+    );
+  }
+}
+
+export  {RadioButton}
+export default RadioControl
